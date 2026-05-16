91527cc6c50c1fa97f3fb12ee22e46eb0f8bfd21
Saitejalukka <lukkasaiteja2002@gmail.com>
2026-05-16 09:14:20 +0530

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,7 @@
+import { bootstrapApplication } from '@angular/platform-browser';
+import { AppComponent } from '../src/app/app';
+import { provideHttpClient, withFetch } from '@angular/common/http';
+
+bootstrapApplication(AppComponent, {
+  providers: [provideHttpClient(withFetch())],
+});
